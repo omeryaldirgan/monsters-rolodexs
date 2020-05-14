5a8792b41e0d51164fc48f99b6bad9e5385e3e31
Ömer Yaldırgan <omeryaldirgan@hotmail.com>
2020-05-14 22:22:25 +0300

    card component yapıldı

---
+++ src/components/card/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './card.css'
+export const Card=(props)=>{
+    const {id,name,email}=props.monster;
+    return(
+        <div className="card-container">
+            <img alt={name} src={`https://robohash.org/${id}?set=set4`}/>
+            <h3>{name}</h3>
+            <span>{email}</span>
+        </div>
+    )
+}
